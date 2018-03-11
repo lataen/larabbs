986d1f286e52b54d152958fcb3a0f6eb6e8f5b8f
vagrant <vagrant@homestead>
2018-03-11 05:00:45 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')"/>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -16,21 +17,21 @@
 </head>
 
 <body>
-    <div id="app" class="{{ route_class() }}-page">
+<div id="app" class="{{ route_class() }}-page">
 
-        @include('layouts._header')
+    @include('layouts._header')
 
-        <div class="container">
-            @include('layouts._message')
-            @yield('content')
+    <div class="container">
+        @include('layouts._message')
+        @yield('content')
 
-        </div>
-
-        @include('layouts._footer')
     </div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    @yield('scripts')
+    @include('layouts._footer')
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
