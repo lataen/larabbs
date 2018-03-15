4e730568b994a6881a2dc3f05689b3a4ce658a64
vagrant <vagrant@homestead>
2018-03-15 12:04:35 +0000

    Integrate laravel-permission package

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -29,8 +30,7 @@ class UsersTableSeeder extends Seeder
             ->times(10)
             ->make()
             ->each(function ($user, $index)
-            use ($faker, $avatars)
-            {
+            use ($faker, $avatars) {
                 // 从头像数组中随机取出一个并赋值
                 $user->avatar = $faker->randomElement($avatars);
             });
@@ -45,8 +45,15 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'licheng';
         $user->email = '1150400220@qq.com';
-        $user->password=bcrypt('123456');
+        $user->password = bcrypt('123456');
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
